c63073f37ace25cef4a502b3f6031cc826a32b2a
chinqan <chinqan@gmail.com>
2026-03-13 17:40:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Database = require('better-sqlite3');
 const path = require('path');
 
 const app = express();
-const PORT = 7866;
+const PORT = 7860;
 
 // Middleware
 app.use(express.json());
